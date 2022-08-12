2377cd6193da1c91558d252eadf7f132f99da179
Mateusgc01 <mateusgc14@gmail.com>
2022-08-12 00:23:33 -0300

    Criação da classe de teste

---
+++ Coordenadas/src/Testando.java
@@ -0,0 +1,16 @@
+public class Testando {
+
+    /**
+     * Teste da lista
+     */
+    public static void main(String args[]) {
+
+        List lista = new List<Integer>();
+        lista.inserir(4);
+        lista.inserir(5);
+        lista.print();
+        lista.inserir(45);
+        lista.print();
+
+    }
+}
